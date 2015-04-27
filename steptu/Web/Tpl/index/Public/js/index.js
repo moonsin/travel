848f0fb17afd940892fd3aa5cd5b85099f426eb6
moon <moonsin@lookout.com>
2015-04-27 15:54:17 +0800

    添加了滚动箭头效果

--- steptu/Web/Tpl/index/Public/js/index.js
+++ steptu/Web/Tpl/index/Public/js/index.js
@@ -23,6 +23,7 @@ function ISL_GoUp(){ //上翻开始
  clearInterval(AutoPlayObj);
  MoveLock = true;
  MoveTimeObj = setInterval('ISL_ScrUp();',Speed);
+document.images['zuoguntu'].src='../Public/images/zuogun1.png'
 }
 function ISL_StopUp(){ //上翻停止
  clearInterval(MoveTimeObj);
@@ -33,6 +34,18 @@ function ISL_StopUp(){ //上翻停止
   MoveLock = false;
  }
  AutoPlay();
+ document.images['zuoguntu'].src='../Public/images/zuogun.png'
+}
+function ISL_StopUp1(){ //上翻停止
+ clearInterval(MoveTimeObj);
+ if(GetObj('ISL_Cont').scrollLeft % PageWidth - fill != 0){
+  Comp = fill - (GetObj('ISL_Cont').scrollLeft % PageWidth);
+  CompScr();
+ }else{
+  MoveLock = false;
+ }
+ AutoPlay();
+ document.images['zuoguntu'].src='../Public/images/zuogun1.png'
 }
 function ISL_ScrUp(){ //上翻动作
  if(GetObj('ISL_Cont').scrollLeft <= 0){GetObj('ISL_Cont').scrollLeft = GetObj('ISL_Cont').scrollLeft + GetObj('List1').offsetWidth}
@@ -45,6 +58,7 @@ function ISL_GoDown(){ //下翻
  MoveLock = true;
  ISL_ScrDown();
  MoveTimeObj = setInterval('ISL_ScrDown()',Speed);
+ document.images['youguntu'].src='../Public/images/yougun1.png'
 }
 function ISL_StopDown(){ //下翻停止
  clearInterval(MoveTimeObj);
@@ -55,7 +69,20 @@ function ISL_StopDown(){ //下翻停止
   MoveLock = false;
  }
  AutoPlay();
+ document.images['youguntu'].src='../Public/images/yougun.png'
 }
+function ISL_StopDown1(){ //下翻停止
+ clearInterval(MoveTimeObj);
+ if(GetObj('ISL_Cont').scrollLeft % PageWidth - fill != 0 ){
+  Comp = PageWidth - GetObj('ISL_Cont').scrollLeft % PageWidth + fill;
+  CompScr();
+ }else{
+  MoveLock = false;
+ }
+ AutoPlay();
+ document.images['youguntu'].src='../Public/images/yougun1.png'
+}
+
 function ISL_ScrDown(){ //下翻动作
  if(GetObj('ISL_Cont').scrollLeft >= GetObj('List1').scrollWidth){GetObj('ISL_Cont').scrollLeft = GetObj('ISL_Cont').scrollLeft - GetObj('List1').scrollWidth;}
  GetObj('ISL_Cont').scrollLeft += Space ;
@@ -85,5 +112,38 @@ function CompScr(){
   setTimeout('CompScr()',Speed);
  }
 }
+function myformatter(date){
+ var y = date.getFullYear();
+var m = date.getMonth()+1;
+var d = date.getDate();
+return y+'-'+(m<10?('0'+m):m)+'-'+(d<10?('0'+d):d);
+ }
+ function myparser(s){
+ if (!s) return new Date();
+ var ss = (s.split('-'));
+ var y = parseInt(ss[0],10);
+ var m = parseInt(ss[1],10);
+var d = parseInt(ss[2],10);
+ if (!isNaN(y) && !isNaN(m) && !isNaN(d)){
+return new Date(y,m-1,d);
+ }else{
+ return new Date();
+ }
+ }
+
+
+
+  var speed=200
+   demo2.innerHTML=demo1.innerHTML
+   function Marquee(){
+   if(demo2.offsetTop-demo.scrollTop<=0)
+   demo.scrollTop-=demo1.offsetHeight
+   else{
+   demo.scrollTop++
+   }
+   }
+   var MyMar=setInterval(Marquee,speed)
+   demo.onmouseover=function() {clearInterval(MyMar)}
+   demo.onmouseout=function() {MyMar=setInterval(Marquee,speed)}
 //--><!]]>
 //--><!]]>
