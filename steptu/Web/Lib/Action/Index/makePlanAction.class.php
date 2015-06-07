516d89915c51c27470129f92231c15c6d2337213
mryung <2327542415@qq.com>
2015-06-08 00:13:21 +0800

    计划

--- steptu/Web/Lib/Action/Index/makePlanAction.class.php
+++ steptu/Web/Lib/Action/Index/makePlanAction.class.php
@@ -4,12 +4,86 @@
     //发起活动模块
 
 
-	class makePlanAction extends Action{
+class makePlanAction extends Action{
 
-		public function makePlan(){
-			$this->display();
+
+	
+	public function makePlan(){	
+		if(IS_POST){
+
+			$shifa = I('shifadi');
+			$mudi = I('mudidi');
+			$huodongshijian =I('huodongshijian');
+			$fabushijian = I('fabushijian');
+
+			$data = array(
+				'startarea' =>$shifa,
+				// 'startTime' => $fabushijian,
+
+				// 'intentarea' => $mudidi,
+				// 'publishtime' => ''
+				);
+			import('ORG.Util.Page');
+			$count = M('action')->where($data)->count();
+			$page = new Page($count,6);
+			$limit = $page->firstRow .','. $page->listRows;
+
+			$content = M('action')->order('id ASC')->limit($limit)->where($data)->select();
+			$this->content = $content;
+			$this->page = $page->show(); 
+
+		}else{
+
+			import('ORG.Util.Page');
+			$count = M('action')->where($data)->count();
+			$page = new Page($count,6);
+			$limit = $page->firstRow .','. $page->listRows;
+
+			$content = M('action')->order('id ASC')->limit($limit)->where($data)->select();
+			$this->content = $content;
+			$this->page = $page->show(); 
+
 		}
+		$this->display();
 	}
+
+	function searchPlan(){
+		$id = I("id");
+		$data = M('action')->find($id);
+		$this->ajaxReturn($data);
+	}
+	public function savePlan(){
+		if(!IS_POST) _404('请先登录');
+		if($_COOKIE['name']){
+
+		$starttime = I('mphuodongshijian');
+		$startarea = I('mpshifadi');
+		$des = I('mpmudidi');
+		$content = I('mpcontent');
+		$max = I('mpcanyurenshu');
+		$faqi = I('mpfaqiren');
+		$beizhu = I('mpother');
+
+		$data = array(
+			'startTime' => $starttime,
+			'startarea' => $startarea,
+			'intentarea' => $des,
+			'content' => $content,
+			'maxnumber' => $max,
+			'initiator' => $faqi,
+			'remark' => $beizhu,
+			'publishtime' => date('Y-m-d')				
+			);
+		$mk = M('action');
+		$mk->add($data);
+		$this->redirect('Index/makePlan/makePlan');
+	}else{
+		$this->error('请先登录');
+	}
+	}
+
+
+}
 
 
 ?>
