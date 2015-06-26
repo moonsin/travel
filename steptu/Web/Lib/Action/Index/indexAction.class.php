34b3a71f080c4e16d1870ea9295ab7137fc918a2
Zhou <517048600@qq.com>
2015-06-26 21:01:17 +0800

    首页酒店

--- steptu/Web/Lib/Action/Index/indexAction.class.php
+++ steptu/Web/Lib/Action/Index/indexAction.class.php
@@ -6,7 +6,30 @@ class indexAction extends Action{
 
 	//显示主页
 	public function index(){
+		$hotel = M('hotel')->where(array('status'=>1))->limit("5")->order("id DESC")->select();
+		$this->assign('hotel',$hotel);
+		if(IS_POST){
+		$mudidi = I('destination');
+		$ruzhu = I('intime');
+		$lidian = I('outtime');
+		$brand = I('brand');
+		$data['status']=1;
+		if($mudidi!=''){
+			$data['destination'] = array('LIKE',"%". I('destination')."%");
+		}
+		if($brand!=''){
+			$data['hotelName'] = array('LIKE',"%". I('brand')."%");
+		}
+		if($ruzhu!=''){
+			$data['startTime'] = array('EGT',I('intime'));
+		}
+		if($jiezhiri!=''){
+			$data['endTime'] = array('ELT',I('outtime'));
+		}
 
+		$hotel = M('hotel')->where($data)->order("id DESC")->limit("5")->select();
+		$this->assign('hotel',$hotel);
+	}
 		$this->assign('note',$this->travelNote());
 		$this->assign('season',$this->discountSeason());
 		$this->display();
@@ -32,7 +55,7 @@ class indexAction extends Action{
 
 	//搜索旁边的酒店信息
 	public function discountHotel(){
-		
+
 	}
 
 }
